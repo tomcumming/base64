8208a8f86d39e95b9a040b3675435b043fa10d7c
Tom Cumming <mail@tomcumming.co.uk>
2025-06-06 21:58:34 +0100

    Add more testing

--- main.c
+++ main.c
@@ -5,54 +5,104 @@
 
 #include "base64.h"
 
-const size_t testCount = 3;
+void pretty_buffer(size_t len, uint8_t *str) {
+  for (size_t i = 0; i < len; i += 1)
+    printf("0x%02X ", str[i]);
+}
 
-uint8_t numbers[256] = {
-    1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,
-    16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,
-    31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,
-    46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,
-    61,  62,  63,  64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,
-    76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,
-    91,  92,  93,  94,  95,  96,  97,  98,  99,  100, 101, 102, 103, 104, 105,
-    106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
-    121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135,
-    136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150,
-    151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165,
-    166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180,
-    181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195,
-    196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210,
-    211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225,
-    226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240,
-    241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
-    0};
+void test_string(size_t len, uint8_t *str) {
+  size_t enc_bufLength = b64_encodedLength(len);
+  size_t dec_bufLength = b64_decodedLength(enc_bufLength);
 
-uint8_t *testCases[] = {(uint8_t *)"Hello World!",
-                        (uint8_t *)"Testing one two three", numbers};
+  uint8_t *enc_buf = malloc(enc_bufLength);
+  uint8_t *dec_buf = malloc(dec_bufLength);
 
-int main(int argc, char **argv) {
-  for (size_t i = 0; i < testCount; i++) {
-    printf("Test #%zu: ", i);
+  b64_encode(str, len, enc_buf);
+  size_t decodedLength = b64_decode(enc_buf, enc_bufLength, dec_buf);
 
-    size_t inputLength = strlen((char *)testCases[i]);
-    size_t encBufLength = b64_encodedLength(inputLength);
-    size_t decBufLength = b64_decodedLength(encBufLength);
+  if (len != decodedLength || memcmp(str, dec_buf, len)) {
+    printf("Mismatch buffers of length %lu, %lu\n", len, decodedLength);
+    printf("in:\t");
+    pretty_buffer(len, str);
+    printf("\n");
+    printf("dec:\t");
+    pretty_buffer(decodedLength, dec_buf);
+    printf("\n");
+    printf("enc: %s\n", enc_buf);
+    exit(1);
+  }
 
-    uint8_t *encBuf = malloc(encBufLength);
-    uint8_t *decBuf = malloc(decBufLength);
+  free(enc_buf);
+  free(dec_buf);
+}
 
-    b64_encode(testCases[i], inputLength, encBuf);
-    size_t decodedLength = b64_decode(encBuf, encBufLength, decBuf);
+void wikipedia_tests() {
+  char *test_inputs[] = {"Man", "Ma", "M"};
+  char *test_output[] = {"TWFu", "TWE=", "TQ=="};
 
-    if (inputLength == decodedLength &&
-        memcmp(testCases[i], decBuf, inputLength) == 0) {
-      printf("pass\n");
-    } else {
-      printf("fail!\n");
+  size_t test_count = sizeof(test_inputs) / sizeof(char *);
+
+  for (size_t i = 0; i < test_count; i += 1) {
+    char *input = test_inputs[i];
+    char *expected = test_output[i];
+
+    printf("Checking encode(%s)\t=\t%s\n", input, expected);
+
+    size_t len = strlen(input);
+    size_t enc_bufLength = b64_encodedLength(len);
+
+    uint8_t *enc_buf = malloc(enc_bufLength);
+
+    b64_encode((uint8_t *)input, len, enc_buf);
+
+    if (memcmp(expected, enc_buf, strlen(expected))) {
+      printf("FAIL\n");
+      exit(1);
     }
 
-    free(encBuf);
-    free(decBuf);
+    free(enc_buf);
+  }
+}
+
+void test_random_string(int len, uint8_t *buf) {
+  for (size_t j = 0; j < len; j += 1)
+    buf[j] = rand();
+
+  test_string(len, buf);
+}
+
+void fuzz(int len) {
+  size_t FUZZ_ITERS = 2 << 20;
+
+  uint8_t *buf = malloc(len);
+
+  for (size_t i = 0; i < FUZZ_ITERS; i += 1)
+    test_random_string(len, buf);
+
+  free(buf);
+}
+
+int main(int argc, char **argv) {
+
+  wikipedia_tests();
+
+  size_t MAX_TEST_LEN = 16;
+  printf("Fuzzing lengths: ");
+  for (size_t len = 0; len <= MAX_TEST_LEN; len += 1) {
+    printf("%lu ", len);
+    fflush(stdout);
+    fuzz(len);
+  }
+  printf("\n");
+
+  size_t LONG_STRING_SIZES[] = {2 << 10, 2 << 20, 2 << 25};
+
+  for (size_t i = 0; i < sizeof(LONG_STRING_SIZES) / sizeof(size_t); i += 1) {
+    size_t len = LONG_STRING_SIZES[i];
+    printf("Testing random string of length %lu\n", len);
+    uint8_t *buf = malloc(len);
+    test_random_string(len, buf);
+    free(buf);
   }
 
   return 0;
